b0d6f35679efe4f7a7455e553ae7914bc54bc1ac
Matthias Roggo <matthias.roggo@gmail.com>
2011-11-15 09:36:09 +0100

    Korrekte Wandpotentialformel

--- load_map.m
+++ load_map.m
@@ -1,29 +1,41 @@
-% Bild Einlesen
-subplot(1,2,1);
-X = imread('maps/testmap.png');
-X = sum(X, 3)/size(X,3); %Grauskala
-imsize = size(X);
+% Parameter
 
-image(X); axis equal; colormap('bone');
+meter = 40; % * pixel, according to:
+map = 'testmap.png';
+
+R = 0.2 * meter; % 
+
+Hue_goal = [0.3 0.1]; % Value / Tolerance
+
+% Read image
+subplot(1,2,1);
+X = imread(['maps/' map]);
+
+% Create layers
+X_hsv = rgb2hsv(X);
+X_gs = sum(X, 3)/size(X,3); % Greyscale
+
+X_goal  = X_hsv(:,:,2) > 0.9 ... % sat
+        & X_hsv(:,:,1) < Hue_goal(1)+Hue_goal(2) ... % hue max
+        & X_hsv(:,:,1) > Hue_goal(1)-Hue_goal(2); % hue min
+
+imsize = size(X);
+image(X_gs); axis equal; colormap('bone');
 
 % Fouriertransformation
-F = fftshift(fft2(X));
-%subplot(1,2,2);
-%image(log(abs(F))); colormap('bone');
+F = fftshift(fft2(X_gs));
 
 m = imsize(1);
 n = imsize(2);
 
-% Filter berechnen
+% Calculate filter and convolution
 [l,k] = meshgrid(1:m,1:n);
 l_0 = m/2;
 k_0 = n/2;
-sigma = 5;
-g_e = exp(-(((k-k_0).^2+(l-l_0).^2)/(2*sigma^2)));
-g_kegel = sqrt( (m/2).^2 + (n/2).^2 ) - sqrt( (l - m/2).^2 + (k - n/2).^2 );
-F = F .* g_e;
 
-X = real(ifft2(ifftshift(F)));
+g_walls = exp( -sqrt( (k-k_0).^2+(l-l_0).^2 )/R );
+
+X_walls = real(ifft2(ifftshift(F .* g_walls)));
 
 subplot(1,2,2);
-mesh(X); colormap('bone');
+mesh(X_walls); colormap('bone');
