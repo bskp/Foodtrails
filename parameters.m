d807d918716acf0d1d77cb5e6d87767e7fd0f7c3
Michael Aebli <schnipper@gmail.com>
2011-11-18 14:40:36 +0100

    global parameters added and parameters.m modified

--- parameters.m
+++ parameters.m
@@ -1,5 +1,20 @@
-% Parameters
-global meter hue_goal map_file R;
+%%%%%%%%%%%%%%%%%%%%
+% GLOBAL PARAMETERS
+%%%%%%%%%%%%%%%%%%%%
+
+global ...
+    dt ... 
+    meter...
+    hue_goal ...
+    map_file ...
+    R ... 
+    v0_alphabeta ...
+    sigma ...
+    agent_number;
+
+%General
+dt=1;
+agent_number=50;
 
 
 % Map
@@ -12,3 +27,7 @@ hue_goal = [0.3 0.1]; % Value / Tolerance
 
 % Boundary potential
 R = 0.2 * meter; % according to paper
+
+% Agents
+v0_alphabeta=2.1;
+sigma=0.3;
