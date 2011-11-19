e1741d2a12175955d0640237182372ae3e20d602
Matthias Roggo <matthias.roggo@gmail.com>
2011-11-19 16:46:51 +0100

    Potentialfeld erstellen klappt einigermassen
    Die einzelnen Zielflächen werden aber noch nicht getrennt auf mehrere
    Ebenen, und die Steilheit ist noch nicht "Paper-konform".
    Die gültigen Startbereiche werden aber korrekt mitrausgegeben.

--- load_map.m
+++ load_map.m
@@ -1,4 +1,4 @@
-function [ map map_init ] = load_map( )
+function [ map, map_init ] = load_map( )
 
 % Generates a potential field, which represents the 
 % boundary and goal forces.
@@ -22,40 +22,59 @@ n = size(X, 2);
 X_hsv = rgb2hsv(X);
 X_gs = sum(X, 3)/size(X,3); % Greyscale
 
-
 % Goals
 X_goal  = X_hsv(:,:,2) > 0.9 ... % sat
         & X_hsv(:,:,1) < hue_goal(1)+hue_goal(2) ... % hue max
         & X_hsv(:,:,1) > hue_goal(1)-hue_goal(2); % hue min
 
-% Init positions
+    
+    
+    
+% Init areas
 X_init  = X_hsv(:,:,2) > 0.9 ... % sat
         & X_hsv(:,:,1) < hue_init(1)+hue_init(2) ... % hue max
         & X_hsv(:,:,1) > hue_init(1)-hue_init(2); % hue min
     
-if ( max(max( X_init )) == 0) % if there are no spots
+
+% Wall potential: treat init- and goal-areas as free space
+X_walls = X_gs;
+X_walls(X_init | X_goal ) = 255 ; 
+
+if ( max(max( X_init )) == 0) % if there are no init spots
     X_init = X_hsv(:,:,3) < 1; % use free space as x_init
 end
 
 
-%% Calculate filter and convolution
+%% Calculate filters and convolutions
 
 % Fourier transformation
-F = fftshift(fft2(X_gs));
+F_walls = fftshift(fft2(X_walls));
 
-% Filter
+% Filter for walls
 [l,k] = meshgrid(1:m,1:n);
 l_0 = m/2;
 k_0 = n/2;
 
 g_walls = exp( -sqrt( (k-k_0).^2+(l-l_0).^2 )/R );
 
-% Convolution
-X_walls = real(ifft2(ifftshift(F .* g_walls)));
+% Filter for goal attraction
+r = ceil( sqrt(m^2 + n^2) ); % Calculate minimal filter radius
 
+[l,k] = meshgrid(1:(2*r),1:(2*r));
+
+m_goal = 1000;
+g_goal = 1/m_goal * sqrt( (k-r).^2+(l-r).^2 ); % create cone
+g_goal = g_goal(r, 1) - g_goal; % shift cone
+g_goal(g_goal<0) = 0; % saturate negative values
+
+% Convolution
+X_walls_conv = real(ifft2(ifftshift(F_walls .* g_walls))); % cyclic
+
+X_goal_conv = conv2(double(X_goal), g_goal, 'same'); % zero-padded
 
 %% Arrange output 
 
-map = [ X_init X_walls ];
+map = [ X_goal_conv + X_walls_conv ];
+map_init = X_init;
 
 end
--- test_load_map.m
+++ test_load_map.m
@@ -1,9 +1,13 @@
 
 parameters; 
 
-map = load_map();
+[ map, map_init ] = load_map();
 
 %subplot(1,2,1);
 %image(X_gs); axis equal; colormap('bone');
 
-mesh(-map); colormap('bone');
+mesh( -map ); colormap('bone');
+
+%subplot(1,2,2);
+
+%mesh( X_goal_cconv ); colormap('bone');
