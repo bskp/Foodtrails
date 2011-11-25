195ce9a5f66fed45331d93f690ee7a7e03887080
Matthias Roggo <matthias.roggo@gmail.com>
2011-11-25 11:25:51 +0100

    hübsch, geht!

--- draw_map_agents.m
+++ draw_map_agents.m
@@ -7,10 +7,15 @@ global map_pretty;
 % 
 % 
 % 
-    imagesc(map_pretty);        %Hintergrundbild laden
+    imagesc( map_pretty );        %Hintergrundbild laden
     colormap('bone');
-% 
-    hold on;  
+    
+    hold on;
+    
+    % draw vector field
+    draw_field = 2;
+    quiver(1:10:300, 1:10:300, fields_x(1:10:300,1:10:300,draw_field), ...
+                               fields_y(1:10:300,1:10:300,draw_field));
     
     quiver(A(1,:),A(2,:),A(3,:),A(4,:));    %Pfeile zeichnen
     
