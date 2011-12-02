4cbbd9862f074da508a760590f22f874adbc3790
Matthias Roggo <matthias.roggo@gmail.com>
2011-12-02 09:59:58 +0100

    nochn'detail: simulate.

--- simulate_v1.m
+++ simulate_v1.m
@@ -44,15 +44,12 @@ A( A(2,:)<1 ) = 1;
 A( A(2,:)>299 ) = 299;
 
 % Find Agents on target areas
-
 for agentID = 1:size(A,2)
-    
    %           (X,             Y,            Target layer);
    if ( X_goals(ceil(A(2, agentID)), ceil(A(1,agentID)), A(6, agentID) ) )
        % agent reached his target
        A(1, agentID) = randi(300,1,1);
        A(2, agentID) = randi(300,1,1);
-       A(6, agentID)
    end
 end
 
