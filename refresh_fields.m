af1c47dcb6dbae63bd3b00d0da265d768e8dcc1b
Matthias Roggo <matthias.roggo@gmail.com>
2011-12-10 01:54:56 +0100

    Hauptsächlich Parameteranpassungen

--- refresh_fields.m
+++ refresh_fields.m
@@ -1,5 +1,5 @@
 % Fast marching algorithm
-X_fm = 0.0001 + X_walls*0.5;
+X_fm = 0.00001 + X_walls*0.9;
 
 X_people = 0*X_walls;
 a_pos = round(A(2,:)) + map_x * round(A(1,:)); % liste der pixel der a.
@@ -11,7 +11,7 @@ g_people = exp( -sqrt( (k-k_0).^2+(l-l_0).^2 )/R_p );
 
 X_people_conv = conv2(X_people, g_people, 'same');
 
-X_fm = X_fm - X_people_conv/30;
+X_fm = X_fm - X_people_conv/15;
 
 %%
 addpath fm/;
