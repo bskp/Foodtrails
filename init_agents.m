a96342d411e0681fbc5bce02b73b5599c59d293b
Moritz Vifian <mo@etx-public-dock-6-dhcp.ethz.ch>
2011-12-15 15:53:28 +0100

    ToDoListe für Bericht, A9 A8 V

--- init_agents.m
+++ init_agents.m
@@ -13,7 +13,8 @@ function Anew=init_agents(agentID,A)
     % 5 desired speed v0|
     % 6 type            |
     % 7 last counter    |
-    % 8 red-carpet-time | (indicates the frame when the agent actually
+    % 8 Fast Marching Estimated Time remaining
+    % 9 red-carpet-time | (indicates the frame when the agent actually
     %                      left the red area; thus "entered" the mensa)
     %
     % PARAMETERS:
@@ -84,7 +85,8 @@ function Anew=init_agents(agentID,A)
     % Random Goal from 2 until n_goals, the first goal is the cash point
     Anew(6,:)=randi(n_goals-1,1,a_num)+1;
     Anew(7,:) = 0;
-    Anew(8,:) = -1;
+    Anew(8,:) = Inf;
+    Anew(9,:) = -1;
     
     if(nargin~=0) 
         A(:,agentID)=Anew;
